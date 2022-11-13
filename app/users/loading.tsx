a1e27e9a2ea95ff070a9d72b6eeec06a43ad39ed
Adam Haubenstock <aqeh@novonordisk.com>
2022-11-13 17:54:22 +0100

    Add retrieval of user data with loading screen

---
+++ app/users/loading.tsx
@@ -0,0 +1,3 @@
+export default function() {
+    return (<div>LOL OMG!!! I AM LIKE TOTALLY LOADING!</div>)
+}
--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,3 +1,11 @@
-export default function Page() {
-  return <div>USERS</div>;
+import users from "../../users.json";
+
+async function getUsers() {
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+  return users;
+}
+
+export default async function Page() {
+  const users = await getUsers();
+  return <div>{JSON.stringify(users)}</div>;
 }
